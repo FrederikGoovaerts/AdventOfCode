530ad7c1fdfb653e827ad5bc4bbbb3d4823f3c6c
Frederik Goovaerts <info@frederik.dev>
2020-12-22 00:31:52 +0100

    Finish day 20 part 2

---
+++ utils/utils.go
@@ -0,0 +1,10 @@
+package utils
+
+// Reverse an incoming string with no special encoding
+func Reverse(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
