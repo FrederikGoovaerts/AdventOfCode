fdff610ae04d5c5adf564c146d91f93179302e11
Frederik Goovaerts <info@frederik.dev>
2022-12-05 12:40:10 +0100

    Add solution to day 5 part 1

---
+++ day5/solution.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+// To make life easier, hardcode the amount of stacks
+const NB_STACK = 9
+
+type Stack struct {
+	crates []string
+}
+
+func (s *Stack) addToTop(c string) {
+	s.crates = append(s.crates, c)
+}
+
+func (s *Stack) removeFromTop() string {
+	removed := s.crates[len(s.crates)-1]
+	s.crates = s.crates[:len(s.crates)-1]
+	return removed
+}
+
+func (s *Stack) addToBottom(c string) {
+	s.crates = append([]string{c}, s.crates...)
+}
+
+type Move struct {
+	amount int
+	from   int
+	to     int
+}
+
+func part1(stacks []Stack, moves []Move) string {
+	for _, move := range moves {
+		for i := 0; i < move.amount; i++ {
+			moved := stacks[move.from-1].removeFromTop()
+			stacks[move.to-1].addToTop(moved)
+		}
+		// fmt.Println(stacks)
+	}
+	result := ""
+	for _, stack := range stacks {
+		result += stack.crates[len(stack.crates)-1][1:2]
+	}
+	return result
+}
+
+func main() {
+	dat, err := os.ReadFile("input")
+	if err != nil {
+		panic(err)
+	}
+
+	lines := strings.Split(string(dat), "\n")
+	stacks := []Stack{}
+	moves := []Move{}
+	for i := 0; i < NB_STACK; i++ {
+		stacks = append(stacks, Stack{})
+	}
+
+	readMovesMode := false
+	for _, line := range lines {
+		if line == "" {
+			continue
+		} else if strings.HasPrefix(line, " 1") {
+			readMovesMode = true
+		} else if !readMovesMode {
+			startSpacingRegex := regexp.MustCompile(`^    `)
+			otherSpacingRegex := regexp.MustCompile(`    `)
+			startFixed := startSpacingRegex.ReplaceAllString(line, "--- ")
+			fixed := otherSpacingRegex.ReplaceAllString(startFixed, " ---")
+
+			crates := strings.Split(fixed, " ")
+			for i := 0; i < NB_STACK; i++ {
+				crate := crates[i]
+				if crate != "---" {
+					stacks[i].addToBottom(crate)
+				}
+			}
+		} else {
+			r, _ := regexp.Compile("move (.*) from (.*) to (.*)")
+			match := r.FindStringSubmatch(line)
+			amount, _ := strconv.Atoi(match[1])
+			from, _ := strconv.Atoi(match[2])
+			to, _ := strconv.Atoi(match[3])
+			moves = append(moves, Move{amount, from, to})
+		}
+	}
+	// fmt.Println(stacks)
+	fmt.Println(part1(stacks, moves))
+	// fmt.Println(part2(pairs))
+}
