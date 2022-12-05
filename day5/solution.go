f2693382f0a5e17d716225f69f9a6ad1f4ece1c6
Frederik Goovaerts <info@frederik.dev>
2022-12-05 12:48:23 +0100

    Add solution to day 5 part 2

--- day5/solution.go
+++ day5/solution.go
@@ -15,13 +15,13 @@ type Stack struct {
 	crates []string
 }
 
-func (s *Stack) addToTop(c string) {
-	s.crates = append(s.crates, c)
+func (s *Stack) addToTop(c []string) {
+	s.crates = append(s.crates, c...)
 }
 
-func (s *Stack) removeFromTop() string {
-	removed := s.crates[len(s.crates)-1]
-	s.crates = s.crates[:len(s.crates)-1]
+func (s *Stack) removeFromTop(amount int) []string {
+	removed := s.crates[len(s.crates)-amount:]
+	s.crates = s.crates[:len(s.crates)-amount]
 	return removed
 }
 
@@ -38,10 +38,21 @@ type Move struct {
 func part1(stacks []Stack, moves []Move) string {
 	for _, move := range moves {
 		for i := 0; i < move.amount; i++ {
-			moved := stacks[move.from-1].removeFromTop()
+			moved := stacks[move.from-1].removeFromTop(1)
 			stacks[move.to-1].addToTop(moved)
 		}
-		// fmt.Println(stacks)
+	}
+	result := ""
+	for _, stack := range stacks {
+		result += stack.crates[len(stack.crates)-1][1:2]
+	}
+	return result
+}
+
+func part2(stacks []Stack, moves []Move) string {
+	for _, move := range moves {
+		moved := stacks[move.from-1].removeFromTop(move.amount)
+		stacks[move.to-1].addToTop(moved)
 	}
 	result := ""
 	for _, stack := range stacks {
@@ -57,10 +68,14 @@ func main() {
 	}
 
 	lines := strings.Split(string(dat), "\n")
-	stacks := []Stack{}
+	// We build the internal data twice to not resort to weird deep copy logic
+	stacksPart1 := []Stack{}
+	stacksPart2 := []Stack{}
+
 	moves := []Move{}
 	for i := 0; i < NB_STACK; i++ {
-		stacks = append(stacks, Stack{})
+		stacksPart1 = append(stacksPart1, Stack{})
+		stacksPart2 = append(stacksPart2, Stack{})
 	}
 
 	readMovesMode := false
@@ -79,7 +94,8 @@ func main() {
 			for i := 0; i < NB_STACK; i++ {
 				crate := crates[i]
 				if crate != "---" {
-					stacks[i].addToBottom(crate)
+					stacksPart1[i].addToBottom(crate)
+					stacksPart2[i].addToBottom(crate)
 				}
 			}
 		} else {
@@ -91,7 +107,7 @@ func main() {
 			moves = append(moves, Move{amount, from, to})
 		}
 	}
-	// fmt.Println(stacks)
-	fmt.Println(part1(stacks, moves))
-	// fmt.Println(part2(pairs))
+
+	fmt.Println(part1(stacksPart1, moves))
+	fmt.Println(part2(stacksPart2, moves))
 }
