70c7f15fc556245db69ccb31798b0d5aac7db45b
Frederik Goovaerts <info@frederik.dev>
2022-12-09 15:44:23 +0100

    Add solution to day 9 part 2

--- day9/solution.go
+++ day9/solution.go
@@ -9,11 +9,49 @@ import (
 
 // For reference: we see right as X+ and up as Y+
 
+// EX1
+// const MIN_X = 0
+// const MAX_X = 5
+// const MIN_Y = 0
+// const MAX_Y = 5
+
+// EX2
+// const MIN_X = -11
+// const MAX_X = 14
+// const MIN_Y = -5
+// const MAX_Y = 15
+
+// func visualize(ropeX []int, ropeY []int) {
+// 	for y := MAX_Y; y >= MIN_Y; y-- {
+// 		for x := MIN_X; x <= MAX_X; x++ {
+// 			found := false
+// 			for id := 0; id < len(ropeX); id++ {
+// 				if ropeX[id] == x && ropeY[id] == y {
+// 					found = true
+// 					fmt.Print(id)
+// 					break
+// 				}
+// 			}
+// 			if !found {
+// 				fmt.Print(".")
+// 			}
+// 		}
+// 		fmt.Println()
+// 	}
+// 	fmt.Println()
+// }
+
 var empty = struct{}{}
 
-func Abs(x int) int {
+func abs(x int) int {
 	if x < 0 {
 		return -x
+	}
+	return x
+}
+func clampToOne(x int) int {
+	if x != 0 {
+		return x / abs(x)
 	}
 	return x
 }
@@ -43,42 +81,26 @@ func getHeadMove(direction string) (int, int) {
 	}
 }
 
-func transSyncDiff(headX int, headY int, tailX int, tailY int, direction string) (int, int) {
-	switch direction {
-	case "R":
-		return 0, headY - tailY
-	case "L":
-		return 0, headY - tailY
-	case "U":
-		return headX - tailX, 0
-	case "D":
-		return headX - tailX, 0
-	default:
-		panic("illegal direction")
-	}
-}
-
-func part1(moves []Move) int {
+func solve(moves []Move, ropeLength int) int {
 	tailLocations := make(map[string]struct{})
-	headX := 0
-	headY := 0
-	tailX := 0
-	tailY := 0
+	ropeX := make([]int, ropeLength)
+	ropeY := make([]int, ropeLength)
 
 	for _, move := range moves {
 		xDiff, yDiff := getHeadMove(move.direction)
 		for i := 0; i < move.distance; i++ {
 			// Move head
-			headX += xDiff
-			headY += yDiff
-			// Drag tail
-			if Abs(tailX-headX) > 1 || Abs(tailY-headY) > 1 {
-				syncX, syncY := transSyncDiff(headX, headY, tailX, tailY, move.direction)
-				tailX += xDiff + syncX
-				tailY += yDiff + syncY
+			ropeX[0] += xDiff
+			ropeY[0] += yDiff
+			// Drag tails
+			for i := 1; i < ropeLength; i++ {
+				if abs(ropeX[i]-ropeX[i-1]) > 1 || abs(ropeY[i]-ropeY[i-1]) > 1 {
+					ropeX[i] += clampToOne(ropeX[i-1] - ropeX[i])
+					ropeY[i] += clampToOne(ropeY[i-1] - ropeY[i])
+				}
 			}
 			// Note tail location
-			addTailLocation(tailX, tailY, &tailLocations)
+			addTailLocation(ropeX[ropeLength-1], ropeY[ropeLength-1], &tailLocations)
 		}
 
 	}
@@ -102,5 +124,6 @@ func main() {
 		}
 	}
 
-	fmt.Println(part1(moves))
+	fmt.Println(solve(moves, 2))
+	fmt.Println(solve(moves, 10))
 }
