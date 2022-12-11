71d6e0fdc76d3dcba6e44bff5a92888c07b4cf1c
Frederik Goovaerts <info@frederik.dev>
2022-12-11 15:36:22 +0100

    Add solution to day 11 part 2

--- day11/solution.go
+++ day11/solution.go
@@ -49,9 +49,36 @@ func part1(monkeys []Monkey) int {
 	return monkeyBusiness[len(monkeys)-1] * monkeyBusiness[len(monkeys)-2]
 }
 
+func part2(monkeys []Monkey) int {
+	monkeyBusiness := make([]int, len(monkeys))
+
+	divisorProduct := 1
+	for _, monkey := range monkeys {
+		divisorProduct *= monkey.check
+	}
+
+	for round := 1; round <= 10000; round++ {
+		for id, monkey := range monkeys {
+			monkeyBusiness[id] += len(monkey.items)
+
+			for len(monkey.items) > 0 {
+				item := monkey.pop()
+				monkeys[id] = monkey
+				updated := monkey.op(item) % divisorProduct
+				if updated%monkey.check == 0 {
+					monkeys[monkey.trueDest].addItem(updated)
+				} else {
+					monkeys[monkey.falseDest].addItem(updated)
+				}
+			}
+		}
+	}
+
+	sort.Ints(monkeyBusiness)
+	return monkeyBusiness[len(monkeys)-1] * monkeyBusiness[len(monkeys)-2]
+}
+
 func main() {
-	monkeys := getInput()
-
-	fmt.Println(part1(monkeys))
-
+	fmt.Println(part1(getInput()))
+	fmt.Println(part2(getInput()))
 }
