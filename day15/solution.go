b252e600838d2a84cb3d65db0609e708d075e4ad
Frederik Goovaerts <info@frederik.dev>
2022-12-15 13:58:01 +0100

    Add solution to day 15 part 2

--- day15/solution.go
+++ day15/solution.go
@@ -18,17 +18,44 @@ func (s *Sensor) covers(x int, y int) bool {
 	return manhattanDistance(x, y, s.x, s.y) <= s.coveredDist
 }
 
+func (s *Sensor) cover(y int, xCovers [][]int) [][]int {
+	spread := s.coveredDist - util.Abs(y-s.y)
+	if spread < 0 {
+		return xCovers
+	}
+	xStart := s.x - spread
+	xEnd := s.x + spread
+
+	result := make([][]int, 0)
+
+	for _, xCover := range xCovers {
+		if xCover[0] >= xStart && xCover[1] <= xEnd {
+			// fully covered
+		} else if xCover[1] < xStart || xCover[0] > xEnd {
+			result = append(result, xCover)
+		} else if xCover[0] > xStart && xCover[1] > xEnd {
+			result = append(result, []int{xEnd + 1, xCover[1]})
+		} else if xCover[0] < xStart && xCover[1] < xEnd {
+			result = append(result, []int{xCover[0], xStart - 1})
+		} else {
+			result = append(result, []int{xCover[0], xStart - 1}, []int{xEnd + 1, xCover[1]})
+		}
+	}
+
+	return result
+}
+
 func manhattanDistance(x1, y1, x2, y2 int) int {
 	return util.Abs(x2-x1) + util.Abs(y2-y1)
 }
 
+// For example
 // const PART1_Y = 10
 
 const PART1_Y = 2_000_000
 
 func part1(sensors []Sensor, beacons map[string]struct{}, xStart int, xEnd int) int {
 	result := 0
-	fmt.Println(xStart, xEnd, beacons)
 
 	for x := xStart; x <= xEnd; x++ {
 		if _, ok := beacons[util.SerializeCoord(x, PART1_Y)]; ok {
@@ -41,14 +68,38 @@ func part1(sensors []Sensor, beacons map[string]struct{}, xStart int, xEnd int) 
 			}
 		}
 	}
-	fmt.Println()
 
 	return result
+}
+
+// For example
+// const PART2_SIZE = 20
+
+const PART2_SIZE = 4_000_000
+
+func part2(sensors []Sensor) int {
+	for y := 0; y <= PART2_SIZE; y++ {
+		uncovered := [][]int{{0, PART2_SIZE}}
+		for _, sensor := range sensors {
+			uncovered = sensor.cover(y, uncovered)
+
+			if len(uncovered) == 0 {
+				break
+			}
+		}
+
+		if len(uncovered) == 1 && uncovered[0][0] == uncovered[0][1] {
+			return uncovered[0][0]*4_000_000 + y
+		}
+	}
+
+	return -1
 }
 
 var inputRegex = regexp.MustCompile("Sensor at x=(.*), y=(.*): closest beacon is at x=(.*), y=(.*)")
 
 func main() {
+	// lines := util.FileAsLines("ex1")
 	lines := util.FileAsLines("input")
 
 	sensors := make([]Sensor, 0)
@@ -81,4 +132,5 @@ func main() {
 	}
 
 	fmt.Println(part1(sensors, beaconSet, smallestXCovered, largestXCovered))
+	fmt.Println(part2(sensors))
 }
