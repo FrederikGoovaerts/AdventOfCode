8dd0c8b8ee5d0fcc0fdfef79165be9f5095070e1
Frederik Goovaerts <info@frederik.dev>
2022-12-18 16:12:43 +0100

    Add tests for utils

---
+++ util/math_test.go
@@ -0,0 +1,50 @@
+package util
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMinInt(t *testing.T) {
+	assert.Equal(t, 2, MinInt(2, 4))
+	assert.Equal(t, 3, MinInt(3, 3))
+}
+
+func TestMaxInt(t *testing.T) {
+	assert.Equal(t, 4, MaxInt(2, 4))
+	assert.Equal(t, 3, MaxInt(3, 3))
+}
+
+func TestAbs(t *testing.T) {
+	assert.Equal(t, 2, Abs(2))
+	assert.Equal(t, 0, Abs(0))
+	assert.Equal(t, 5, Abs(-5))
+}
+
+func TestClampToOne(t *testing.T) {
+	assert.Equal(t, 1, ClampToOne(15))
+	assert.Equal(t, 0, ClampToOne(0))
+	assert.Equal(t, -1, ClampToOne(-7))
+}
+
+func TestSerializeCoord(t *testing.T) {
+	assert.Equal(t, "1 2", SerializeCoord(1, 2))
+}
+
+func TestDeserializeCoord(t *testing.T) {
+	x, y := DeserializeCoord("1 2")
+	assert.Equal(t, 1, x)
+	assert.Equal(t, 2, y)
+}
+
+func TestSerializeCoord3(t *testing.T) {
+	assert.Equal(t, "1 2 4", SerializeCoord3(Coord3{1, 2, 4}))
+}
+
+func TestDeserializeCoord3(t *testing.T) {
+	coord3 := DeserializeCoord3("1 2 4")
+	assert.Equal(t, 1, coord3.X)
+	assert.Equal(t, 2, coord3.Y)
+	assert.Equal(t, 4, coord3.Z)
+}
