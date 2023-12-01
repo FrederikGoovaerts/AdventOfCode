3db35c4812d203f134e308f61bd0a21c3a402d1a
Frederik Goovaerts <frederik@imengine.be>
2023-12-01 07:39:32 +0100

    Add solution to day 1

--- 2023/src/1/solution.ts
+++ 2023/src/1/solution.ts
@@ -1,6 +1,40 @@
 import { sum } from "lodash";
-import { asNumberList } from "../utils/inputReader";
+import { asList } from "../utils/inputReader";
 
-const input = asNumberList();
+const input = asList();
 
-console.log(sum(input));
+const values: number[] = [];
+
+for (const line of input) {
+  const digitOnlyLine = line.replaceAll(/[a-z]/g, "");
+  const digits = digitOnlyLine[0] + digitOnlyLine[digitOnlyLine.length - 1];
+  values.push(parseInt(digits));
+}
+console.log(sum(values));
+
+const numberMap: Record<string, string> = {
+  one: "1",
+  two: "2",
+  three: "3",
+  four: "4",
+  five: "5",
+  six: "6",
+  seven: "7",
+  eight: "8",
+  nine: "9",
+};
+
+const newValues: number[] = [];
+const regex = /(?=([1-9]|one|two|three|four|five|six|seven|eight|nine))/g;
+
+for (const line of input) {
+  const matches = [...line.matchAll(regex)]
+
+    const first = matches.at(0)![1];
+    const last = matches.at(-1)![1];
+    const newValue = parseInt(
+      (numberMap[first] ?? first) + (numberMap[last] ?? last)
+    );
+    newValues.push(newValue);
+}
+console.log(sum(newValues));
