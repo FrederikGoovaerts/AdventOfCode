6bb14744c7398257955dbdc11e308abb910a69e5
Frederik Goovaerts <info@frederik.dev>
2023-12-17 10:22:36 +0100

    Add solution to day 17 part 2

--- 2023/17/solution.ts
+++ 2023/17/solution.ts
@@ -17,8 +17,6 @@ interface Node {
   ldstep: number;
 }
 
-const start: Node = { x: 0, y: 0, loss: 0, ld: "right", ldstep: 0 };
-
 function stringify(coords: {
   x: number;
   y: number;
@@ -28,46 +26,58 @@ function stringify(coords: {
   return `${coords.x}|${coords.y}|${coords.ld}|${coords.ldstep}`;
 }
 
-const currentBest = new Map<string, number>();
-currentBest.set(stringify(start), 0);
+function getMinHeatLoss(
+  minSteps: number | undefined,
+  maxSteps: number
+): number {
+  const start: Node = { x: 0, y: 0, loss: 0, ld: "right", ldstep: 0 };
+  const currentBest = new Map<string, number>();
+  currentBest.set(stringify(start), 0);
 
-const nodes = new MinHeap<Node>((a, b) => a.loss - b.loss);
-nodes.insert(start);
+  const nodes = new MinHeap<Node>((a, b) => a.loss - b.loss);
+  nodes.insert(start);
 
-while (nodes.size() > 0) {
-  const curr = nodes.removeMin()!;
-  if (curr.x === goal.x && curr.y === goal.y) {
-    console.log(curr);
-    break;
-  }
-  const ns = getDirectionNeighboursLocs(curr.x, curr.y, input, false);
-  for (const [direction, coords] of Object.entries(ns)) {
-    if (
-      (curr.ld === direction && curr.ldstep === 3) ||
-      curr.ld === reverseDirection(direction as Direction)
-    ) {
-      continue;
-    } else {
-      const neighbourLoss = input[coords.y][coords.x];
-      const next = {
-        x: coords.x,
-        y: coords.y,
-        loss: curr.loss + neighbourLoss,
-        ld: direction as Direction,
-        ldstep: direction === curr.ld ? curr.ldstep + 1 : 1,
-      };
-
-      const bestForNeighbour = currentBest.get(stringify(next));
-
+  while (nodes.size() > 0) {
+    const curr = nodes.removeMin()!;
+    if (curr.x === goal.x && curr.y === goal.y) {
+      return curr.loss;
+    }
+    const ns = getDirectionNeighboursLocs(curr.x, curr.y, input, false);
+    for (const [direction, coords] of Object.entries(ns)) {
       if (
-        bestForNeighbour === undefined ||
-        curr.loss + neighbourLoss < bestForNeighbour
+        (curr.ld === direction && curr.ldstep === maxSteps) ||
+        (minSteps !== undefined &&
+          curr.ld !== direction &&
+          curr.ldstep < minSteps) ||
+        curr.ld === reverseDirection(direction as Direction)
       ) {
-        nodes.insert(next);
-        if (curr.ld !== direction || curr.ldstep < 3) {
-          currentBest.set(stringify(next), curr.loss + neighbourLoss);
+        continue;
+      } else {
+        const neighbourLoss = input[coords.y][coords.x];
+        const next = {
+          x: coords.x,
+          y: coords.y,
+          loss: curr.loss + neighbourLoss,
+          ld: direction as Direction,
+          ldstep: direction === curr.ld ? curr.ldstep + 1 : 1,
+        };
+
+        const bestForNeighbour = currentBest.get(stringify(next));
+
+        if (
+          bestForNeighbour === undefined ||
+          curr.loss + neighbourLoss < bestForNeighbour
+        ) {
+          nodes.insert(next);
+          if (curr.ld !== direction || curr.ldstep < maxSteps) {
+            currentBest.set(stringify(next), curr.loss + neighbourLoss);
+          }
         }
       }
     }
   }
+  return -1;
 }
+
+console.log(getMinHeatLoss(undefined, 3));
+console.log(getMinHeatLoss(4, 10));
