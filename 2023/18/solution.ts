59f9ba0c7644a5602d262776f0a98360dd984b08
Frederik Goovaerts <frederik@imengine.be>
2023-12-18 19:18:49 +0100

    Add solution to day 18 part 2

--- 2023/18/solution.ts
+++ 2023/18/solution.ts
@@ -1,192 +1,162 @@
 import { asList } from "../utils/inputReader";
-import { Direction, getNeighboursLocs } from "../utils/neighbours";
+import { Direction } from "../utils/neighbours";
 
 const input = asList("input");
 
-type Dir = "R" | "D" | "L" | "U";
-
-function getDirOffset(dir: Dir): { x: number; y: number } {
-  switch (dir) {
-    case "R":
-      return { x: 1, y: 0 };
-    case "D":
-      return { x: 0, y: 1 };
-    case "L":
-      return { x: -1, y: 0 };
-    case "U":
-      return { x: 0, y: -1 };
-  }
+interface Instruction {
+  dist: number;
+  dir: Direction;
 }
 
-let maxX = 0;
-let maxY = 0;
-
-function createCcwPath(): { x: number; y: number }[] {
-  const result: { x: number; y: number }[] = [];
-  let currX = 0;
-  let currY = 0;
-  let minX = 0;
-  let minY = 0;
-
-  result.push({ x: currX, y: currY });
-
-  for (const line of input) {
-    const split = line.split(" ");
-    const direction = split[0] as Dir;
-    const dirOffset = getDirOffset(direction);
-    const dist = parseInt(split[1]);
-    for (let i = 0; i < dist; i++) {
-      currX += dirOffset.x;
-      currY += dirOffset.y;
-      if (!result.some((p) => p.x === currX && p.y === currY)) {
-        result.push({ x: currX, y: currY });
-      }
-      minX = Math.min(minX, currX);
-      minY = Math.min(minY, currY);
-      maxX = Math.max(maxX, currX);
-      maxY = Math.max(maxY, currY);
-    }
-  }
-
-  for (const el of result) {
-    el.x -= minX;
-    el.y -= minY;
-  }
-  maxX -= minX;
-  maxY -= minY;
-
-  return result;
+interface DistAndCorners {
+  prevTurn: Turn;
+  dist: number;
+  nextTurn: Turn;
+  name: string;
 }
 
-const path = createCcwPath().reverse();
+type Turn = "CW" | "CCW";
 
-const cleanMap: ("." | "P" | "I" | "O")[][] = [];
-
-// This starts out as the first F corner we encounter
-let currPathElem = {
-  y: 0,
-  x: Math.min(...path.filter((p) => p.y === 0).map((p) => p.x)),
-};
-
-for (let y = 0; y <= maxY; y++) {
-  const line: ("." | "P" | "I" | "O")[] = [];
-  for (let x = 0; x <= maxX; x++) {
-    if (path.some((p) => p.x === x && p.y === y)) {
-      line.push("P");
-    } else {
-      line.push(".");
-    }
-  }
-  cleanMap.push(line);
-}
-
-// We'll traverse the path starting from a corner and looking down
-let lookDir: Direction = "down";
-
-const firstCornerIndex = path.findIndex(
-  (p) => p.x === currPathElem.x && p.y === currPathElem.y
-);
-
-const applyPathToMap = (el: { x: number; y: number }, dir: Direction) => {
-  switch (dir) {
-    case "up":
-      {
-        if (cleanMap[el.y]?.[el.x + 1] === ".") {
-          cleanMap[el.y][el.x + 1] = "O";
-        }
-        if (cleanMap[el.y]?.[el.x - 1] === ".") {
-          cleanMap[el.y][el.x - 1] = "I";
-        }
-      }
-      break;
-    case "down":
-      {
-        if (cleanMap[el.y]?.[el.x + 1] === ".") {
-          cleanMap[el.y][el.x + 1] = "I";
-        }
-        if (cleanMap[el.y]?.[el.x - 1] === ".") {
-          cleanMap[el.y][el.x - 1] = "O";
-        }
-      }
-      break;
-    case "left":
-      {
-        if (cleanMap[el.y - 1]?.[el.x] === ".") {
-          cleanMap[el.y - 1][el.x] = "O";
-        }
-        if (cleanMap[el.y + 1]?.[el.x] === ".") {
-          cleanMap[el.y + 1][el.x] = "I";
-        }
-      }
-      break;
-    case "right":
-      {
-        if (cleanMap[el.y - 1]?.[el.x] === ".") {
-          cleanMap[el.y - 1][el.x] = "I";
-        }
-        if (cleanMap[el.y + 1]?.[el.x] === ".") {
-          cleanMap[el.y + 1][el.x] = "O";
-        }
-      }
-      break;
-  }
-};
-
-applyPathToMap(currPathElem, lookDir);
-
-let last = currPathElem;
-for (
-  let i = (firstCornerIndex + 1) % path.length;
-  i !== firstCornerIndex;
-  i = (i + 1) % path.length
-) {
-  currPathElem = path[i];
-  if (currPathElem.x > last.x) {
-    lookDir = "right";
-  } else if (currPathElem.x < last.x) {
-    lookDir = "left";
-  } else if (currPathElem.y < last.y) {
-    lookDir = "up";
-  } else if (currPathElem.y > last.y) {
-    lookDir = "down";
-  }
-
-  applyPathToMap(currPathElem, lookDir);
-  const next = path[(i + 1) % path.length];
-  let dirToNext: Direction;
-  if (next.x > currPathElem.x) {
-    dirToNext = "right";
-  } else if (next.x < currPathElem.x) {
-    dirToNext = "left";
-  } else if (next.y < currPathElem.y) {
-    dirToNext = "up";
+function dirsToTurn(from: Direction, to: Direction): Turn {
+  if (
+    (from === "up" && to === "right") ||
+    (from === "right" && to === "down") ||
+    (from === "down" && to === "left") ||
+    (from === "left" && to === "up")
+  ) {
+    return "CW";
   } else {
-    dirToNext = "down";
+    return "CCW";
   }
-
-  applyPathToMap(currPathElem, dirToNext);
-
-  last = currPathElem;
 }
 
-// Flood fill I
-for (let y = 0; y < cleanMap.length; y++) {
-  for (let x = 0; x < cleanMap[0].length; x++) {
-    if (cleanMap[y][x] === "I") {
-      for (const n of getNeighboursLocs(x, y, cleanMap, false)) {
-        if (cleanMap[n.y][n.x] === ".") {
-          cleanMap[n.y][n.x] = "I";
+const dirMap: Record<string, Direction> = {
+  D: "down",
+  U: "up",
+  L: "left",
+  R: "right",
+};
+
+const dirMapNumbers: Record<string, Direction> = {
+  "1": "down",
+  "3": "up",
+  "2": "left",
+  "0": "right",
+};
+
+function solve(instructions: Instruction[]) {
+
+  const turns: DistAndCorners[] = [];
+
+  for (let i = 0; i < instructions.length; i++) {
+    const prev = instructions.at(i - 1)!;
+    const curr = instructions.at(i)!;
+    const next = instructions.at((i + 1) % instructions.length)!;
+
+    turns.push({
+      prevTurn: dirsToTurn(prev.dir, curr.dir),
+      dist: curr.dist,
+      nextTurn: dirsToTurn(curr.dir, next.dir),
+      name: `${i}`,
+    });
+  }
+
+  const cwOverCount =
+    turns.filter((t) => t.nextTurn === "CW").length -
+    turns.filter((t) => t.nextTurn === "CCW").length;
+
+  const clockWisePath = (
+    cwOverCount === 4
+      ? turns
+      : turns.reverse().map(
+          (v, i): DistAndCorners => ({
+            dist: v.dist,
+            prevTurn: v.prevTurn === "CW" ? "CCW" : "CW",
+            nextTurn: v.nextTurn === "CW" ? "CCW" : "CW",
+            name: `${i}`,
+          })
+        )
+  ).map((v) => {
+    let newDist = v.dist;
+    if (v.prevTurn === "CW" && v.nextTurn === "CW") {
+      newDist++;
+    }
+    if (v.prevTurn === "CCW" && v.nextTurn === "CCW") {
+      newDist--;
+    }
+    return {
+      ...v,
+      dist: newDist,
+    };
+  });
+
+
+  let addedSpace = 0;
+  let removedSpace = 0;
+
+  while (clockWisePath.length > 4) {
+    for (let i = 0; i < clockWisePath.length; i++) {
+      const prev = clockWisePath.at(i - 1)!;
+      const curr = clockWisePath.at(i)!;
+      const next = clockWisePath.at((i + 1) % clockWisePath.length)!;
+
+      if (curr.nextTurn === curr.prevTurn) {
+        const shift = Math.min(prev.dist, next.dist);
+        if (curr.nextTurn === "CW") {
+          removedSpace += shift * curr.dist;
+        } else {
+          addedSpace += shift * curr.dist;
         }
+        prev.dist -= shift;
+        next.dist -= shift;
+      }
+    }
+
+    if (clockWisePath[0].dist === 0) {
+      const last = clockWisePath.pop()!;
+      clockWisePath.unshift(last);
+    }
+
+    let i = 1;
+
+    while (i < clockWisePath.length - 1) {
+      const curr = clockWisePath[i];
+      if (curr.dist === 0 && curr.nextTurn !== curr.prevTurn) {
+        const prev = clockWisePath.at(i - 1)!;
+        const next = clockWisePath.at((i + 1) % clockWisePath.length)!;
+        clockWisePath.splice(i - 1, 3, {
+          nextTurn: next.nextTurn,
+          dist: prev.dist + next.dist,
+          prevTurn: prev.prevTurn,
+          name: `${prev.name}|${curr.name}|${next.name}`,
+        });
+      } else {
+        i++;
       }
     }
   }
+
+  console.log(
+    removedSpace - addedSpace + clockWisePath[0].dist * clockWisePath[1].dist
+  );
 }
 
-let iCount = 0;
-for (let y = 0; y < cleanMap.length; y++) {
-  // This can be used to visually debug
-  // console.log(cleanMap[y].join(""));
-  iCount += cleanMap[y].filter((v) => v === "I").length;
-}
+const basicInstructions = input.map((line): Instruction => {
+  const split = line.split(" ");
+  return {
+    dist: parseInt(split[1]),
+    dir: dirMap[split[0]],
+  };
+});
 
-console.log(iCount + path.length);
+solve(basicInstructions);
+
+const largeInstructions = input.map((line): Instruction => {
+  const hex = line.split(" ")[2].slice(2);
+  return {
+    dist: Number(`0x${hex.slice(0, 5)}`),
+    dir: dirMapNumbers[hex[5]],
+  };
+});
+
+solve(largeInstructions);
